7b23017a27bcda9acda96abae707953888a31afc
Michael Villalobos <michael.villalobos@zefr.com>
2014-09-08 16:09:03 -0700

    flaskified site

--- main_site/__init__.py
+++ main_site/__init__.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about/index.html')
+
+
+@app.route('/about/villalobos')
+def aboutVillalobos():
+    return render_template('about/villalobos.html')
+
+
+@app.route('/client')
+def client():
+    return render_template('client/index.html')
+
+
+@app.route('/press')
+def press():
+    return render_template('press/index.html')
+
+
+@app.route('/contact')
+def contact():
+	return render_template('contact/index.html')
