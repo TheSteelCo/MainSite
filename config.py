e11657dbe4f1285517656e958f83ccc3a1f1863b
Michael Villalobos <michael.villalobos@zefr.com>
2014-10-28 10:49:24 -0700

    more fixes

--- config.py
+++ config.py
@@ -2,10 +2,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'mysql://dbsteelco:Royal72uk@thesteelco.com/thesteelco'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'mysql://dbsteelco:Royal72uk@thesteelco.com/thesteelco'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
 
--- runp-mysql.fcgi
+++ runp-mysql.fcgi
@@ -2,7 +2,7 @@
 import os
 
 # use mysql
-os.environ['DATABASE_URL'] = 'mysql://apps:apps@localhost/apps'
+os.environ['DATABASE_URL'] = 'mysql://dbsteelco:Royal72uk@thesteelco.com/thesteelco'
 
 from flipflop import WSGIServer
 from main_site import app
